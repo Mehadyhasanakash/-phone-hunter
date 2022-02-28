ebb5f4bad1da95ea7828262e5d083e5b71532a9f
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-02-28 23:48:28 +0600

    myfrist-commit

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('this is me')
+
+
+// search & inputFiled && json convert javaScript
+const phoneHunter = ()=>{
+    const inputFiled = document.getElementById('input-filed').value;
+    const inputFiledText = parseFloat(inputFiled);
+    //     console.log(inputFiled);
+    // inputFiledText.value ='';
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputFiled}`
+    fetch(url)
+    .then(response => response)
+    .then(data => phoneItem(data))
+    
+}
+
+const phoneItem = (phonItem)=>{
+    // console.log(phonItem);
+}
