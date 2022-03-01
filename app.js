10adfb269044ad50014cec2661e2b8cb3a62b334
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-03-01 20:51:12 +0600

    7th-commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const phoneHunter = ()=>{
 
 const phoneItem = (datas)=>{
     
-    console.log(datas)
+    // console.log(datas)
     const mainDaiv = document.getElementById('main');
     // const frist20Data = data.slice(0, 20);
     // console.log(frist20Data);
@@ -24,14 +24,43 @@ const phoneItem = (datas)=>{
         const newDiv = document.createElement('div')
         newDiv.className = ('col-md-4');
         newDiv.innerHTML = `
-        <div class="border border-1 text-center">
+        <div class="border border-1 text-center p-3">
       <img src="${data.image}" alt="">
-      <h4>${data.brand}</h4>
-      <h4>${data.phone_name}</h4>
+      <h4> Brand: ${data.brand}</h4>
+      <h4> Phone Name: ${data.phone_name}</h4>
+      <button onclick="phoneDetails('${data.slug}')" type="button" class="btn btn-success">Phone Details</button>
 
     </div>
         `
         
         mainDaiv.appendChild(newDiv);
     }
+}
+
+const phoneDetails = (id)=>{
+    // console.log(id)
+    
+    const url =`https://openapi.programming-hero.com/api/phone/${id}`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => allPhoneDetail(data.data))
+
+}
+
+const allPhoneDetail = (info)=>{
+
+    console.log(info);
+    const mainDetail = document.getElementById('phone-details');
+    mainDetail.innerHTML =`
+    <div>
+    <img src="${info.image}" alt="">
+    <h4> slug: ${info.slug}</h4>
+    <h4> releaseDate: ${info.releaseDate}</h4>
+    <h4> sensors: ${info.sensors}</h4>
+    <h4> memory: ${info.memory}</h4>
+
+
+    </div>`
+
+
 }
